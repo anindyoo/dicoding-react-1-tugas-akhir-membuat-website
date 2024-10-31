5c6f4ab994f67ad871e45abf80f61700e75b428b
anindyoo <anindyo0216@gmail.com>
2024-10-31 15:02:45 +0700

    feat: add content for intro, background, & tracklist.

--- index.js
+++ index.js
@@ -1,159 +1,354 @@
 const tracklist = [
   {
     id: 1,
-    title: 'AMERIICAN REQUIEM',
+    title: 'AMERIICAN<br/>REQUIEM',
     features: '',
+    font: 'anton',
+    rotate: '-7.21deg',
+    fontSize: '2.5rem',
+    top: '36px',
+    left: '',
+    gap: '',
   },
   {
     id: 2,
     title: 'BLACKBIIRD',
     features: 'Brittney Spencer, Reyna Roberts, Tanner Adell, & Tiera Kennedy',
+    font: 'pm text-black',
+    rotate: '',
+    fontSize: '',
+    top: '56px',
+    left: '',
+    gap: '3.75rem',
   },
   {
     id: 3,
-    title: '16 CARRIAGES',
+    title: '16<br/>CARRIAGES',
     features: '',
+    font: 'anton',
+    rotate: '2.8deg',
+    fontSize: '2.25rem',
+    top: '16px',
+    left: '',
+    gap: '',
   },
   {
     id: 4,
     title: 'PROTECTOR',
     features: 'Rumi Carter',
+    font: 'pm',
+    rotate: '5.65deg',
+    fontSize: '1.5rem',
+    top: '22px',
+    left: '',
+    gap: '2.75rem',
   },
   {
     id: 5,
-    title: 'MY ROSE',
+    title: 'MY<br/>ROSE',
     features: '',
+    font: 'anton',
+    rotate: '-8deg',
+    fontSize: '2.375rem',
+    top: '28px',
+    left: '-1px',
+    gap: '',
   },
   {
     id: 6,
-    title: 'SMOKE HOUR ★ WILLIE NELSON',
+    title: '<span class="font-pm" style="font-size: 0.625rem; text-decoration: underline;">SMOKE&nbsp;</span><div class="font-pm" style="font-size: 0.75rem; margin-top: -0.75rem; margin-left: 2rem; text-decoration: underline;">HOUR&nbsp;</div>WILLIE<br/>&nbsp;&nbsp;NELSON',
     features: 'Willie Nelson',
+    font: 'anton',
+    rotate: '-4deg',
+    fontSize: '1.75rem',
+    top: '',
+    left: '-4px',
+    gap: '',
   },
   {
     id: 7,
-    title: 'TEXAS HOLD \'EM',
+    title: 'TEXAS<br/>HOLD<br/>\'EM',
     features: '',
+    font: 'pm',
+    rotate: '11.56deg',
+    fontSize: '2rem',
+    top: '20px',
+    left: '-4px',
+    gap: '',
   },
   {
     id: 8,
     title: 'BODYGUARD',
     features: '',
+    font: 'anton',
+    rotate: '-11.35deg',
+    fontSize: '1.875rem',
+    top: '36px',
+    left: '',
+    gap: '',
   },
   {
     id: 9,
-    title: 'DOLLY P',
+    title: 'DOLLY<br/>P',
     features: 'Dolly Parton',
+    font: 'pm text-black',
+    rotate: '-14.35deg',
+    fontSize: '1.625',
+    top: '24px',
+    left: '-3px',
+    gap: '2rem',
   },
   {
     id: 10,
     title: 'JOLENE',
     features: '',
+    font: 'pm',
+    rotate: '3.34deg',
+    fontSize: '2.25rem',
+    top: '22px',
+    left: '',
+    gap: '',
   },
   {
     id: 11,
     title: 'DAUGHTER',
     features: '',
+    font: 'anton',
+    rotate: '-5.2deg',
+    fontSize: '1.75rem',
+    top: '31px',
+    left: '',
+    gap: '',
   },
   {
     id: 12,
     title: 'SPAGHETTII',
     features: 'Linda Martel, & Shaboozey',
+    font: 'pm',
+    rotate: '-3.40deg',
+    fontSize: '1.875rem',
+    top: '46px',
+    left: '',
+    gap: '4.25rem',
   },
   {
     id: 13,
-    title: 'ALLIIGATOR TEARS',
+    title: 'ALLIIGATOR<br/>TEARS',
     features: '',
+    font: 'anton text-black',
+    rotate: '-2.8deg',
+    fontSize: '1.375rem',
+    top: '14px',
+    left: '-4px',
+    gap: '',
   },
   {
     id: 14,
-    title: 'SMOKE HOUR II',
+    title: 'SMOKE<br/>HOUR II',
     features: 'Willie Nelson',
+    font: 'anton',
+    rotate: '6deg',
+    fontSize: '2.25rem',
+    top: '21px',
+    left: '',
+    gap: '1.25rem',
   },
   {
     id: 15,
-    title: 'JUST FOR FUN',
+    title: 'JUST<br/>&nbsp;FOR<br/>&nbsp;&nbsp;&nbsp;FUN',
     features: 'Willie Jones',
+    font: 'pm',
+    rotate: '3.34deg',
+    fontSize: '1.625rem',
+    top: '10px',
+    left: '-8px',
+    gap: '0.5rem',
   },
   {
     id: 16,
-    title: 'II MOST WANTED',
+    title: 'II MOST<br/>WANTED&nbsp;&nbsp;',
     features: 'Miley Cyrus',
+    font: 'pm',
+    rotate: '-0.82deg',
+    fontSize: '1.5rem',
+    top: '26px',
+    left: '',
+    gap: '2.75rem',
   },
   {
     id: 17,
-    title: 'LEVII\'S JEANS',
+    title: 'LEVII\'S<br/>JEANS',
     features: 'Post Malone',
+    font: 'anton',
+    rotate: '-13.24deg',
+    fontSize: '2.375rem',
+    top: '26px',
+    left: '-4px',
+    gap: '3.25rem',
   },
   {
     id: 18,
     title: 'FLAMENCO',
     features: '',
+    font: 'pm',
+    rotate: '-0.82deg',
+    fontSize: '1.5rem',
+    top: '28px',
+    left: '',
+    gap: '',
   },
   {
     id: 19,
-    title: 'THE LINDA MARTEL SHOW',
+    title: `
+      <div class="tlms">
+        <div class="font-pm the">
+          THE
+        </div>
+        <div class="lms">
+          LINDA<br/>MARTEL<br/>SHOW
+        </div>
+      </div>
+    `,
     features: 'Linda Martel',
+    font: 'anton',
+    rotate: '',
+    fontSize: '',
+    top: '',
+    left: '',
+    gap: '',
   },
   {
     id: 20,
     title: 'YA YA',
     features: '',
+    font: 'pm',
+    rotate: '2.5deg',
+    fontSize: '2.85rem',
+    top: '38px',
+    left: '3px',
+    gap: '',
   },
   {
     id: 21,
     title: 'OH LOUISIANA',
     features: '',
+    font: 'pm',
+    rotate: '-6.3deg',
+    fontSize: '1.25rem',
+    top: '32px',
+    left: '',
+    gap: '',
   },
   {
     id: 22,
-    title: 'DESERT EAGLE',
+    title: 'DESERT<br/>EAGLE',
     features: '',
+    font: 'anton',
+    rotate: '5.52',
+    fontSize: '1.875rem',
+    top: '15px',
+    left: '-10px',
+    gap: '',
   },
   {
     id: 23,
     title: 'RIIVERDANCE',
     features: '',
+    font: 'pm',
+    rotate: '-7.58deg',
+    fontSize: '1.3rem',
+    top: '22px',
+    left: '',
+    gap: '',
   },
   {
     id: 24,
-    title: 'II HANDS II HEAVEN',
+    title: 'II HANDS<br/>II HEAVEN',
     features: '',
+    font: 'pm',
+    rotate: '-9deg',
+    fontSize: '1.5rem',
+    top: '20px',
+    left: '',
+    gap: '',
   },
   {
     id: 25,
     title: 'TYRANT',
     features: 'Dolly Parton',
+    font: 'anton',
+    rotate: '4.5deg',
+    fontSize: '2rem',
+    top: '24px',
+    left: '',
+    gap: '3.5rem',
   },
   {
     id: 26,
-    title: 'SWEET ★ HONEY ★ BUCKIIN\'',
+    title: 'SWEET<br/>HONEY<br/>BUCKIIN\'',
     features: 'Shaboozey',
+    font: 'pm text-black',
+    rotate: '-3.5deg',
+    fontSize: '1.25rem',
+    top: '22px',
+    left: '-10px',
+    gap: '2rem',
   },
   {
     id: 27,
     title: 'AMEN',
     features: '',
+    font: 'anton',
+    rotate: '2.5deg',
+    fontSize: '2.375rem',
+    top: '34px',
+    left: '',
+    gap: '',
   },
 ];
 
 const generateTracklist = () => {
-  const trackListElements = tracklist.map((track) => {
-    const li = document.createElement('li');    
-    const h3 = document.createElement('h3');
-    h3.textContent = track.title;
-    li.appendChild(h3);
-    
-    if (track.features) {
-      const p = document.createElement('p');
-      p.textContent = `with ${track.features}`;
-      li.appendChild(p);
-    }
-
+  const tracklistElement = document.getElementById('track-list');
+  const liArray = tracklist.map((track) => {
+    const li =
+    `<li
+      class="tracklist-item track-${track.id}"
+      style="${track.id === 100 && 'grid-column: 1 / span 2'};"
+    >
+      <div
+        class="track-detail"
+        style="
+          ${track.gap ? `gap: ${track.gap}` : ''};
+        "
+      >
+        <h3
+          class="font-${track.font}"
+          style="
+            font-size: ${track.fontSize};
+            ${track.rotate ? `rotate: ${track.rotate}` : ''};
+            ${track.top ? `top: ${track.top}` : ''};
+            ${track.left ? `left: ${track.left}` : ''};
+          "
+        >${track.title}</h3>
+        <p>${track.features ? `with ${track.features}` : '' }</p>
+        <img src="./assets/images/tracklist/${track.id}.svg" alt="background for track #${track.id}" />
+      </div>
+    </li>`;
     return li;
-  });
-  return trackListElements;
+  })
+  tracklistElement.innerHTML = liArray.join(' ');
 };
+generateTracklist();
 
-const tracklistElement = document.getElementById('track-list');
-const lis = generateTracklist();
-tracklistElement.append(...lis);
+var scrollPosition = window.scrollY;
+var fixedNav = document.getElementById('header-nav');
+
+window.addEventListener('scroll', function() {
+  scrollPosition = window.scrollY;
+
+  if (scrollPosition >= window.innerHeight - 21) {
+    fixedNav.classList.add('fixed-nav');
+  } else {
+    fixedNav.classList.remove('fixed-nav');
+  }
+});
